8e6b21c65f2af6409dc4942049dd6676bad01de3
Gustav Pursche <gustavpursche@gmail.com>
2014-02-19 18:34:49 +0100

    add documentation, use utc-formats for recent activities

--- sandglass/public/js/collections/activity.js
+++ sandglass/public/js/collections/activity.js
@@ -11,31 +11,33 @@ define([ 'lodash',
 
   var ActivityCollection = Backbone.Collection.extend({
     url: defaults.urlRoot + 'activities/',
-    el: $('.timeline'),
     model: Activity,
 
     initialize: function() {
       this._views = [];
 
+      /* when adding a new model, rerender the timeline */
       this.on('add', function( model ) {
         Sandglass.views.timeline.add( model );
       }.bind( this ));
 
+      /* fetch of a whole new set - complete rerender */
       this.on('reset', function() {
         Sandglass.views.timeline.render();
       });
     },
 
+    /* load activities for a given timerange (default this - 1month) */
     loadRecent: function( from, to ) {
       return new Promise(function( res, rej ) {
         /* default today minus 1 month */
         if( !from ) {
-          from = moment().utc().subtract( 'months', 1 );
+          from = moment().utc().subtract( 'months', 1 ).format();
         }
 
         /* use now as end date */
         if( !to ) {
-          to = moment().utc();
+          to = moment().utc().format();
         }
 
         /* always empty the whole collection, so we call it later with
@@ -44,11 +46,14 @@ define([ 'lodash',
 
         this.fetch({
           /* see #1 */
-          url: defaults.urlRoot + 'users/' + Sandglass.User.get('id') +
-               '/?action=activities&from=' + encodeURIComponent( from.format() )
-               + '&to=' + encodeURIComponent( to.format() ) + '/'
-        })
-          .done( res )
+          url: defaults.urlRoot + 'users/' +
+               Sandglass.User.get('id') +
+               '/' +
+               '?action=activities' +
+               '&from=' + encodeURIComponent( from ) +
+               '&to=' + encodeURIComponent( to ) +
+               '/'
+        }).done( res )
           .fail( rej );
       }.bind( this ));
     }
